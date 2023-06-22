44aba18f170e0464987aff73329ce440514bf607
putrie19 <119852247+putrie19@users.noreply.github.com>
2023-06-22 15:11:34 +0700

    web.php

---
+++ app/Http/Controllers/PeminjamController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Peminjam;
+use Illuminate\Http\Request;
+
+class PeminjamController extends Controller
+{
+    public function index(){
+         $peminjam = Peminjam::all();
+         return view('peminjam.index',compact(['peminjam']));
+    }
+}
