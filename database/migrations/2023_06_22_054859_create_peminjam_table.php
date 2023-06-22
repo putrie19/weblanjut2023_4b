dbd5679490b73c81538d3036c22b5eca0bd8086a
putrie19 <119852247+putrie19@users.noreply.github.com>
2023-06-22 15:56:51 +0700

    peminjam.php

---
+++ database/migrations/2023_06_22_054859_create_peminjam_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeminjamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjam', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->enum('jurusan',['teknik_informatika','industri','teknik_sipil']);
+            $table->string('kode_buku');
+            $table->string('judul_buku');
+            $table->date('tanggal_terbit');
+            $table->date('tanggal_pinjam');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjam');
+    }
+}
---
+++ resources/views/peminjam/index.blade.php
@@ -0,0 +1,20 @@
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>NAMA</th>
+        <th>JURUSAN</th>
+        <th>KODE BUKU</th>
+        <th>JUDUL BUKU</th>
+        <th>TANGGAL TERBIT</th>
+        <th>TANGGAL PINJAM</th>
+    </tr>
+    <tr>
+            <td>1</td>
+            <td>rubi</td>
+            <td>teknik_informatika</td>
+            <td>012</td>
+            <td>kualitas</td>
+            <td>2016-06-16</td>
+            <td>2023-06-23</td>
+        </tr>
+</table>
